0c8f1be405cdd8ccfd6f48ef85ba74cb88961726
Viniciuspra <vinicius.cascaesp@gmail.com>
2023-09-27 23:51:14 -0300

    chore: Increase Button icon size

--- src/components/SocialMediaIcon.tsx
+++ src/components/SocialMediaIcon.tsx
@@ -9,7 +9,7 @@ interface SocialMediaIconProps {
 export function SocialMediaIcon({ imgSrc, title }: SocialMediaIconProps) {
   return (
     <Tooltip title={title} placement="bottom" arrow>
-      <div className="flex items-center justify-center h-12 w-12 rounded-full bg-icons cursor-pointer hover:bg-secondary stroke-secondary transition-all ease-linear duration-200">
+      <div className="flex items-center justify-center h-16 w-16 rounded-full bg-icons cursor-pointer hover:bg-secondary stroke-secondary transition-all ease-linear duration-200">
         <Image src={imgSrc} alt={title} width={48} height={48}/>
       </div>
     </Tooltip>
