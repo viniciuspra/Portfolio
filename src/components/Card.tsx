142ef883fe9a7910f767c8320b9617e730263cbc
Viniciuspra <vinicius.cascaesp@gmail.com>
2023-09-27 23:52:30 -0300

    style: Update hover styling for Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,36 @@
 import Image from "next/image";
 
+import { SocialMediaIcon } from "./SocialMediaIcon";
+
+import githubIcon2 from "@/assets/github-icon2.svg";
+import webLink from "@/assets/web-link-icon.svg";
+
 interface CardProps {
   imgSrc: string;
   title: string;
+  url: string;
+  url_github: string;
 }
 
-export function Card({ imgSrc, title }: CardProps) {
+export function Card({ imgSrc, title, url, url_github }: CardProps) {
   return (
-    <div className="relative group flex items-center justify-center py-8 px-4 bg-accent hover:bg-hoverSecondary flex-1 cursor-pointer h-52 xs:h-72 xs:px-8 min-w-fit sm:min-w-[330px] max-w-[448px] rounded-2xl overflow-hidden transition-all duration-500 ease-in-out">
+    <div className="z-0 relative group flex items-center justify-center aspect-video flex-1 cursor-pointer min-w-fit sm:min-w-[330px] max-w-[448px] rounded-xl overflow-hidden transition-all duration-500 ease-in-out">
       <Image
         src={imgSrc}
         alt={title}
         width={1000}
         height={1000}
-        className="transition-all rounded-lg duration-500 backdrop-blur-0 group-hover:scale-105 ease-in-out max-h-[220px]"
+        className="transition-all duration-700 backdrop-blur-0 group-hover:scale-105 ease-in-out "
       />
-
-      <p className="text-textPrimary font-bold absolute -top-12 left-1/2 -translate-x-1/2 opacity-0 group-hover:top-1 group-hover:opacity-100 transition-all duration-500 ease-in-out text-center w-full px-8 text-base sm:text-lg z-10 [text-shadow:_0_1px_1px_rgb(0_0_0_/_50%)]">
-        {title}
+      <p className="z-20 flex absolute w-full items-center justify-center gap-12 -top-1/2 group-hover:top-1/3 xl:group-hover:top-20 transition-all duration-300">
+        <a href={url_github} target="_blank">
+          <SocialMediaIcon title="Github" imgSrc={githubIcon2} />
+        </a>
+        <a href={url} target="_blank">
+          <SocialMediaIcon title="site" imgSrc={webLink} />
+        </a>
       </p>
+      <div className="z-10 absolute top-0 left-0 w-full h-full group-hover:backdrop-blur-sm" />
     </div>
   );
 }
