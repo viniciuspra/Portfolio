43161fd5942e6b6c4fa338ce842ad909f22c328b
Viniciuspra <vinicius.cascaesp@gmail.com>
2023-09-28 19:06:28 -0300

    feat: Add content to About page

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -1,5 +1,8 @@
 import ContactPageImg from "@/assets/contact-page.svg";
 import ContactPageIllustration from "@/assets/contact-illustration.svg";
+import Image from "next/image";
+import { InputLabel } from "@/components/InputLabel";
+import { Button } from "@/components/Button";
 
 export function Contact() {
   return (
@@ -12,6 +15,41 @@ export function Contact() {
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
       }}
-    ></div>
+    >
+      <div className="max-w-screen-2xl flex flex-col xl:flex-row xl:justify-between items-center xl:items-start gap-12 w-full pt-20 pb-20 sm:pb-16 px-12">
+        <div className="flex-1 flex flex-col gap-4 md:gap-8">
+          <h2 className="text-textPrimary text-center xl:text-start text-4xl sm:text-5xl lg:text-6xl font-bold">
+            Contact <span className="text-secondary ">me</span>
+          </h2>
+
+          <Image
+            src={ContactPageIllustration}
+            alt="illustration image"
+            className="max-w-full sm:max-w-[401px] max-h-[350px]"
+            width={1000}
+            height={1000}
+          />
+        </div>
+
+        <div className="flex-1 flex flex-col gap-6 w-full max-w-[696px]">
+          <div className="flex flex-col sm:flex-row items-center gap-6">
+            <InputLabel labelText="Seu Nome" placeholderText="Nome" />
+            <InputLabel labelText="Seu E-mail" placeholderText="E-mail" />
+          </div>
+
+          <div className="flex flex-col sm:flex-row items-center gap-6">
+            <InputLabel
+              labelText="Sua Mensagem"
+              placeholderText="Mensagem"
+              textarea
+            />
+          </div>
+
+          <Button secondary>Enviar Mensagem</Button>
+        </div>
+      </div>
+
+      <div className="absolute bottom-0 left-0 w-full h-[1px] bg-divider" />
+    </div>
   );
 }
