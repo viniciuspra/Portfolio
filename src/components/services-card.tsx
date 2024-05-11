3e7bb7343db662a78a1738179190b89704bc7848
viniciuspra <vinicius.cascaesp@gmail.com>
2024-05-11 15:33:48 -0300

    fix: Adjust service-card size for mobile and fix border animation

--- src/components/services-card.tsx
+++ src/components/services-card.tsx
@@ -20,7 +20,7 @@ export function ServicesCard({
         hidden: { opacity: 0, translateX: -100 },
         visible: { opacity: 1, translateX: 0 },
       }}
-      className="group relative mx-auto grid h-[200px] w-[300px] cursor-pointer place-items-center rounded-lg border bg-background before:absolute before:-z-10 before:h-[165px] before:rounded-xl hover:before:transition-all sm:h-[165px] sm:w-[450px] md:w-[650px] md:before:w-[650px] md:before:animate-rbg-effect md:hover:before:h-[172px] md:hover:before:w-[657px] md:hover:before:bg-blue-500 lg:w-[750px] lg:before:w-[750px] lg:hover:before:w-[757px] xl:w-[800px] xl:before:w-[800px] xl:hover:before:w-[807px]"
+      className="group relative mx-auto grid h-fit w-full cursor-pointer place-items-center rounded-lg border bg-background py-5 before:absolute before:-z-10 before:h-[165px] before:rounded-xl hover:before:h-[calc(100%+10px)] hover:before:w-[calc(100%+10px)] hover:before:transition-all sm:h-[200px] sm:w-full sm:py-0 md:w-[650px] md:before:w-[650px] md:before:animate-rbg-effect md:hover:before:bg-blue-500 lg:w-[750px] lg:before:w-[750px] xl:w-[800px]"
     >
       <div className="flex h-full w-full flex-col items-start justify-center gap-1.5 px-6">
         <div className="space-y-2">
