68b29919fbf1eaaabf768809e55352f192d720b9
viniciuspra <vinicius.cascaesp@gmail.com>
2024-05-10 21:33:12 -0300

    feat: Create Services Page

--- src/app/(site)/services/page.tsx
+++ src/app/(site)/services/page.tsx
@@ -1,7 +1,29 @@
-export default function ServicesPage() {
+import React from "react";
+import { PageTitle } from "@/components/page-title";
+import { ServicesCard } from "@/components/services-card";
+import { getServices } from "@/sanity/sanity-utils";
+import { DivAnimation } from "@/components/div-animation";
+
+export default async function ServicesPage() {
+  const service = await getServices();
+
   return (
-    <div className="flex h-full flex-col items-center py-20 lg:ml-64">
-      
+    <div className="flex flex-col px-5 md:px-20">
+      <div className="w-full space-y-3 p-2">
+        <PageTitle subtitle={service.description}>{service.title}</PageTitle>
+      </div>
+      <DivAnimation className="space-y-7 py-10" ChildrenAnimation>
+        {service.services.map((service) => (
+          <React.Fragment key={service._key}>
+            <ServicesCard
+              title={service.title}
+              description={service.description}
+              image={service.image}
+              price={service.price}
+            />
+          </React.Fragment>
+        ))}
+      </DivAnimation>
     </div>
   );
 }
