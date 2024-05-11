eaeae9a303032028063402d9f025062d1f465aa1
viniciuspra <vinicius.cascaesp@gmail.com>
2024-05-10 21:32:56 -0300

    refactor: Change title of projects page

--- src/app/(site)/projects/page.tsx
+++ src/app/(site)/projects/page.tsx
@@ -5,10 +5,10 @@ import { ProjectCard } from "@/components/project-card";
 export default async function ProjectsPage() {
   const projects = await getProjects();
   return (
-    <div className="px-5 md:px-20">
+    <div className="flex flex-col px-5 md:px-20">
       <div className="w-full space-y-3 p-2">
-        <PageTitle subtitle="Explore a selection of my projects.">
-          Projects
+        <PageTitle subtitle="Explore a selection of my personal projects.">
+          Personal Projects
         </PageTitle>
       </div>
       <ProjectCard projects={projects} />
