1305c11e9e6d153656132b659ccd62f70c1325fc
viniciuspra <vinicius.cascaesp@gmail.com>
2024-05-14 17:26:06 -0300

    feat: Import remaining schemas

--- src/sanity/schemas/index.ts
+++ src/sanity/schemas/index.ts
@@ -1,8 +1,20 @@
 import page from "@/sanity/schemas/page-schema";
-import project from "@/sanity/schemas/project-schema";
+import { project, projectPage } from "@/sanity/schemas/project-schema";
 import home from "@/sanity/schemas/home-schema";
 import service from "@/sanity/schemas/service-schema";
+import favorites from "@/sanity/schemas/favorites-schema";
+import contact from "@/sanity/schemas/contact-schema";
+import StackPage from "@/sanity/schemas/stack-schema";
 
-const schemas = [project, page, home, service];
+const schemas = [
+  project,
+  page,
+  home,
+  service,
+  projectPage,
+  favorites,
+  contact,
+  StackPage,
+];
 
 export default schemas;
