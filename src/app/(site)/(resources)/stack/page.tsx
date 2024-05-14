f51569e435cb99d0dfe27d850b19648fd3f180f8
viniciuspra <vinicius.cascaesp@gmail.com>
2024-05-14 17:35:44 -0300

    feat: Make stack page dynamic by fetching content from CMS

--- src/app/(site)/(resources)/stack/page.tsx
+++ src/app/(site)/(resources)/stack/page.tsx
@@ -1,103 +1,81 @@
+import { DivAnimation } from "@/components/div-animation";
 import { PageTitle } from "@/components/page-title";
 import { StackCard } from "@/components/stack-card";
+import { getStackData } from "@/sanity/sanity-utils";
 
-export default function StackPage() {
+export default async function StackPage() {
+  const data = await getStackData();
   return (
     <div className="flex w-full flex-col px-5 md:px-10 lg:px-20">
       <div className="w-full space-y-3 py-2">
-        <PageTitle subtitle="Explore a selection of my personal projects.">
-          Stack
-        </PageTitle>
+        <PageTitle subtitle={data.subtitle}>{data.title}</PageTitle>
       </div>
-      <div className="space-y-10">
-        <div className="flex flex-col gap-4">
-          <h3 className="text-2xl font-bold text-blue-100/95 first-letter:uppercase">
-            programming Language
-          </h3>
-          <div className="space-y-3">
-            <StackCard
-              image="https://skillicons.dev/icons?i=js"
-              title="JavaScript"
-            />
-            <StackCard
-              image="https://skillicons.dev/icons?i=ts"
-              title="TypeScript"
-            />
-          </div>
-        </div>
-        <div className="grid gap-10 md:grid-cols-2 md:gap-3">
-          <div className="flex flex-col gap-4">
-            <h3 className="text-2xl font-bold text-blue-100/95 first-letter:uppercase">
-              Front-end
-            </h3>
-            <div className="space-y-3">
-              <StackCard
-                image="https://skillicons.dev/icons?i=html"
-                title="HTML5"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=css"
-                title="CSS3"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=react"
-                title="ReactJs"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=nextjs"
-                title="NextJs"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=tailwindcss"
-                title="Tailwind CSS"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=styledcomponents"
-                title="Styled-Components"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=redux"
-                title="Redux"
-              />
-            </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <h3 className="text-2xl font-bold text-blue-100/95 first-letter:uppercase">
-              Back-end
-            </h3>
-            <div className="space-y-3">
-              <StackCard
-                image="https://skillicons.dev/icons?i=nodejs"
-                title="NodeJs"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=express"
-                title="Express"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=sqlite"
-                title="SQLite"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=postgres"
-                title="PostgreSQL"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=prisma"
-                title="prisma"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=docker"
-                title="Docker"
-              />
-              <StackCard
-                image="https://skillicons.dev/icons?i=jest"
-                title="Jest"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      <DivAnimation className="space-y-10">
+        {data.technologies.map(
+          (category, index) =>
+            index === 0 && (
+              <div key={category.category} className="flex flex-col gap-4">
+                <h3 className="text-2xl font-bold text-blue-100/95 first-letter:uppercase">
+                  {category.category}
+                </h3>
+                <div className="space-y-3">
+                  {category.technologies.map((technology) => (
+                    <StackCard
+                      key={technology.title}
+                      image={technology.image}
+                      title={technology.title}
+                    />
+                  ))}
+                </div>
+              </div>
+            ),
+        )}
+        <DivAnimation
+          duration={0.2}
+          ChildrenAnimation
+          className="grid gap-10 md:grid-cols-2 md:gap-3"
+        >
+          {data.technologies.map(
+            (category) =>
+              category.category === "Front-end" && (
+                <div key={category.category} className="flex flex-col gap-4">
+                  <h3 className="text-2xl font-bold text-blue-100/95 first-letter:uppercase">
+                    {category.category}
+                  </h3>
+                  <div className="space-y-3">
+                    {category.technologies.map((technology) => (
+                      <StackCard
+                        key={technology.title}
+                        image={technology.image}
+                        title={technology.title}
+                      />
+                    ))}
+                  </div>
+                </div>
+              ),
+          )}
+          {data.technologies.map(
+            (category) =>
+              category.category === "Back-end" && (
+                <div key={category.category} className="flex flex-col gap-4">
+                  <h3 className="text-2xl font-bold text-blue-100/95 first-letter:uppercase">
+                    {category.category}
+                  </h3>
+                  <div className="space-y-3">
+                    {category.technologies.map((technology) => (
+                      <StackCard
+                        key={technology.title}
+                        image={technology.image}
+                        title={technology.title}
+                        side="right"
+                      />
+                    ))}
+                  </div>
+                </div>
+              ),
+          )}
+        </DivAnimation>
+      </DivAnimation>
     </div>
   );
 }
