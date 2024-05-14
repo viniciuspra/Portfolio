6c29ec460020ba581edbd14a3473c8d88e9f6335
viniciuspra <vinicius.cascaesp@gmail.com>
2024-05-14 17:23:09 -0300

    refactor: Change type from content to image in Page type

--- src/sanity/schemas/page-schema.ts
+++ src/sanity/schemas/page-schema.ts
@@ -18,12 +18,14 @@ const page = {
       },
     },
     {
-      name: "content",
-      title: "Content",
-      type: "array",
-      of: [{ type: "block" }],
-    }
-  ]
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
 };
 
 export default page;
