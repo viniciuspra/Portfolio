25c8ef2bb3709a74920636c4ff0fde12c065354e
viniciuspra <vinicius.cascaesp@gmail.com>
2024-05-14 17:32:32 -0300

    feat: Add Title and subtitle for projects page

--- src/app/(site)/projects/page.tsx
+++ src/app/(site)/projects/page.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { PageTitle } from "@/components/page-title";
-import { getProjects } from "@/sanity/sanity-utils";
+import { getProjects, getProjectsPage } from "@/sanity/sanity-utils";
 import { ProjectCard } from "@/components/project-card";
+
 export default async function ProjectsPage() {
   const projects = await getProjects();
+  const pageData = await getProjectsPage();
   return (
     <div className="flex flex-col px-5 md:px-20">
       <div className="w-full space-y-3 p-2">
-        <PageTitle subtitle="Explore a selection of my personal projects.">
-          Personal Projects
-        </PageTitle>
+        <PageTitle subtitle={pageData.description}>{pageData.title}</PageTitle>
       </div>
       <ProjectCard projects={projects} />
     </div>
